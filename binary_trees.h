6a4c7560ca407427cfae4bfa74b4a81589c4d516
ZAKARIA <zakariaelaro@gmail.com>
2023-08-30 05:04:05 -0700

    New node Task 0

---
+++ binary_trees.h
@@ -0,0 +1,22 @@
+#ifndef BINARY_TREES_H
+#define BINARY_TREES_H
+
+/* prototypes */
+
+/* Binary tree node structure */
+struct binary_tree_s
+{
+int n;
+struct binary_tree_s *parent;
+struct binary_tree_s *left;
+struct binary_tree_s *right;
+};
+typedef struct binary_tree_s binary_tree_t;
+
+/* Prototype for the binary_tree_node function */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+void binary_tree_print(const binary_tree_t *);
+
+/* Other function prototypes */
+
+#endif /* BINARY_TREES_H */
