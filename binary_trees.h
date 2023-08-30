be4c5f738490c717ce9bb75e0d49db0451ea49e4
ZAKARIA <zakariaelaro@gmail.com>
2023-08-30 06:06:22 -0700

    Insert left Task1

--- binary_trees.h
+++ binary_trees.h
@@ -16,6 +16,7 @@ typedef struct binary_tree_s binary_tree_t;
 /* Prototype for the binary_tree_node function */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 void binary_tree_print(const binary_tree_t *);
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 
 /* Other function prototypes */
 
